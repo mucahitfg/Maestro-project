4ce52ab7be44cbcc15d85aefb9c5840808b47113
mücahit <mucahit@ali-MacBook-Air.local>
2023-04-25 01:06:46 +0300

    navigation added.

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, Text, View } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import BlogScreen from './src/screens/BlogScreen';
+import BlogDetailScreen from './src/screens/BlogDetailScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{flex:1}}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name='Blog' component={BlogScreen} options={{ headerShown: false }} />
+          <Stack.Screen name='Blog Detail' component={BlogDetailScreen} options={{ headerShown: false }} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ src/screens/BlogDetailScreen.js
@@ -0,0 +1,12 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+
+const BlogDetailScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>blogDetailScreen</Text>
+    </SafeAreaView>
+  )
+}
+
+export default BlogDetailScreen
